6c262f547bf90bc7ba71bb8a96e3cb7f4a9e8d02
Brynn Lewin <bhl21@fsu.edu>
2024-09-15 15:46:09 -0400

    everything but music is working

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
 
 let theFont;
 let cyclops;
+let sound;
+
 
 function preload(){
   theFont = loadFont("assets/ProtestGuerrilla-Regular.ttf");
-  cyclops = loadImage ("assets/cyclops.png")
+  cyclops = loadImage ("assets/cyclops.png");
+  sound = loadSound ("assets/sound");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   textFont(theFont);
   textSize(72);
   textAlign(CENTER);
   imageMode(CENTER);
+ 
 
 }
 
 function draw() {
   background("#39FF14");
+
+
+  if (mouseIsPressed) {
+    show3D = true;
   
-  if (mouseIsPressed){
-    image(cyclops, width/2 - 5, height/2 - 5);
   } else {
-  fill(color("white"));
-  text("SUBTRONICS", width/2, height/2); 
+    show3D = false; 
+
+  }
+  
+  if (show3D){
+    push();
+    noStroke();
+    texture(cyclops);
+    rotateY(millis() / 1000); //ROTATE AROUND Y AXSIS 
+    box(400); //box with image 
+    pop();
+  } else {
+    if (keyIsDown(32)){
+      let xOffset = random(-5, 5);
+      let yOffset = random(-5, 5);
+      text("SUBTRONICS", xOffset, yOffset);
+    }
+    text("SUBTRONICS", 0, 0); 
   }
 }
 
