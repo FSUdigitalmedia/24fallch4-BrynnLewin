89c371a01247c511ea02329000bcf43828f8c057
Brynn Lewin <bhl21@fsu.edu>
2024-09-15 14:56:17 -0400

    subtronics with green and white

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
+
+let theFont;
+let cyclops;
+
+function preload(){
+  theFont = loadFont("assets/ProtestGuerrilla-Regular.ttf");
+  cyclops = loadImage ("assets/cyclops.png")
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textFont(theFont);
+  textSize(72);
+  textAlign(CENTER);
+  imageMode(CENTER);
+
 }
 
 function draw() {
-  background(125);
-  circle(width/2, height/2, 40);
+  background("#39FF14");
+  
+  fill(color("white"));
+  text("SUBTRONICS", width/2, height/2); 
 }
+
