526717c2f35932cfed3c5549aa75e2fc9c957c53
Brynn Lewin <bhl21@fsu.edu>
2024-09-15 15:03:25 -0400

    when click image

--- sketch.js
+++ sketch.js
@@ -19,7 +19,11 @@ function setup() {
 function draw() {
   background("#39FF14");
   
+  if (mouseIsPressed){
+    image(cyclops, width/2 - 5, height/2 - 5);
+  } else {
   fill(color("white"));
   text("SUBTRONICS", width/2, height/2); 
+  }
 }
 
